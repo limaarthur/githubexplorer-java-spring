df423761890093ebb2bd72247e4af309e1702951
Arthur Lima <artglima.dev@gmail.com>
2025-01-16 22:13:00 -0300

    feat: first commit

---
+++ src/main/java/com/ignitec/githubexplorer/GithubexplorerApplication.java
@@ -0,0 +1,13 @@
+package com.ignitec.githubexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubexplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubexplorerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ignitec/githubexplorer/GithubexplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ignitec.githubexplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubexplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
